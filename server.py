626e8d6a02e5d93dd1134ef26c40cc42d093dc88
kjco <kjcouyang@gmail.com>
2018-07-18 23:09:37 +0000

    Added functionality: info window with weather details at marker location in map, save time and location to database, user login and register.

--- server.py
+++ server.py
@@ -12,6 +12,7 @@ from datetime import datetime
 
 from geopy.geocoders import Nominatim
 
+import json
 import geojson
 
 from flask_googlemaps import GoogleMaps
@@ -29,6 +30,8 @@ darksky_key=os.environ['DARKSKY_API_KEY']
 
 @app.route("/")
 def index():
+
+    print(session)
 
     return render_template("homepage.html")
 
@@ -71,24 +74,71 @@ def location_info():
 
 @app.route('/markercoord', methods=['POST'])
 def get_post_javascript_data():
-    coord = request.form['coord_data']
-    coord_lst = coord.split(',')
-    coord_lst = [float(item) for item in coord_lst]
-    print("@@@@@@@@@@@@@@@@@@.{}".format(coord_lst))
-    print("!!!!!!!!!!!!!!!!!!{}".format(type(coord_lst[0])))
+    print(request.form)
+    lat = request.form['lat']
+    lng = request.form['lng']
+    print(lat, lng)
 
 
-    loc = Location(lat=coord_lst[0],
-                   lng=coord_lst[1])
+    # coord_lst = coord.split(',')
+    # coord_lst = [float(item) for item in coord_lst]
+    # print("@@@@@@@@@@@@@@@@@@.{}".format(coord_lst))
+    # print("!!!!!!!!!!!!!!!!!!{}".format(type(coord_lst[0])))
+
+
+    loc = Location(lat=float(lat),
+                   lng=float(lng))
     db.session.add(loc)
     db.session.commit()
 
-    current_loc = db.session.query(Location).filter(Location.lat==coord_lst[0],Location.lng==coord_lst[1]).first()
-    print(current_loc)
+    # current_loc = db.session.query(Location).filter(Location.lat==coord_lst[0],Location.lng==coord_lst[1]).first()
+    # print(current_loc)
 
-    return coord
+    return 'Location saved'
 
     
+
+@app.route('/save-json', methods=['POST'])
+def get_save_data():
+    print(request.form)
+    print(request.form['lat'], request.form['lng'], request.form['timestamp'])
+
+    lat = float(request.form['lat'])
+    lng = float(request.form['lng'])
+    timestamp = int(request.form['timestamp'])
+
+    print(lat)
+    print(lng)
+
+    loc1 = db.session.query(Location).filter(Location.lat < lat+0.0001, Location.lat > lat-0.0001, Location.lng < lng+0.0001, Location.lng > lng-0.0001).first()
+    print("**LOC1** {}".format(loc1))
+
+    dt_saved_datetime = datetime.fromtimestamp(timestamp)
+    dt_when_saved = datetime.utcnow()
+
+    print("!!Current time!!: {}".format(datetime.utcnow().timestamp()))
+    print(dt_saved_datetime)
+    print(session['person_id'], type(session['person_id']))
+
+    new_record = SavedRecord(person_id=session['person_id'],
+                             loc_id=loc1.loc_id,
+                             saved_datetime=dt_saved_datetime,
+                             when_saved=dt_when_saved)
+
+    db.session.add(new_record)
+    db.session.commit()
+
+    return 'Location saved'
+
+
+@app.route("/person/<person_id>")
+def person_profile(person_id):
+    person = db.session.query(Person).get(person_id)
+    print(person.saved_records)
+
+    saved_lst = person.saved_records
+
+    return render_template("result.html", saved_lst=saved_lst)
 
 
 
@@ -109,6 +159,49 @@ def get_location_info():
 
     return render_template("result.html", location_dict=location_dict)
 
+
+
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        passw = request.form["passw"]
+        
+        login = Person.query.filter(Person.email==email, Person.password==passw).first()
+        print(login)
+        print(login.password)
+        if login is not None:
+            session['email'] = email
+            session['person_id'] = login.person_id
+            flash("Login successful")
+            return redirect("/")
+        else:
+            flash("Email or password incorrect. Try again.")
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        email = request.form['email']
+        passw = request.form['passw']
+
+        register = Person(email = email, password = passw)
+        db.session.add(register)
+        db.session.commit()
+        flash("You've registered. Please login.")
+
+        return redirect("/login")
+    return render_template("register.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    session.pop('person_id', None)
+    flash("You are logged out.")
+    return redirect('/')
 
 
 
